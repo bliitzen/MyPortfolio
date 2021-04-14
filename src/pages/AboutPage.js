27474d15a79c60c984e7375f9063816e6415c7bf
bliitzen <60658959+bliitzen@users.noreply.github.com>
2021-04-14 15:15:18 -0400

    About page - added my beautiful face

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import Content from '../components/Content';
 
+import myFace from '../assets/images/FacePicture.jpg';
 import Hero from '../components/Hero';
 
 function AboutPage(props) {
@@ -9,6 +10,7 @@ function AboutPage(props) {
     return(
         <div className="orng">
             <Hero title={props.title} />
+            <img src={myFace} alt="myFace" className="face"></img>
             <Content>
                 Welcome to my website! I love to code and have recently taken a strong interest in web design. I have been working in React for a few weeks and this portfolio is my first release of a React website!
                 There are a lot of cool features in React and I can't wait to put my new skills to the test on a bigger project.
